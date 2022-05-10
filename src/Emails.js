c77b845722d51cc89785964421d06a1f005035b1
Chris Hyde <c_hyde333@hotmail.com>
2022-05-10 15:38:45 +0100

    Core Criteria - done

---
+++ src/Emails.js
@@ -0,0 +1,18 @@
+import Email from "./Email";
+
+function Emails(props) {
+  return (
+    <ul>
+      {props.filteredEmails.map((email, index) => (
+        <Email
+          key={index}
+          email={email}
+          toggleRead={props.toggleRead}
+          toggleStar={props.toggleStar}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default Emails;
